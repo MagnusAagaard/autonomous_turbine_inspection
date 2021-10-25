77e3cb528634caae043720e3c02430431a8a4916
Magnus Aagaard <magnus_aagaard@hotmail.com>
2021-10-25 13:35:07 +0200

    g2opy library working. Added template for optimizer script

---
+++ scripts/optimizer.py
@@ -0,0 +1,13 @@
+import g2o
+
+class Optimizer:
+    def __init__(self):
+        # Init
+        pass
+
+def main():
+    # Main
+    print("Hello world!")
+
+if __name__ == "__main__":
+    main()
