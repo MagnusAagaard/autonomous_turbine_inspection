e2ffa4d71265fe85e5d9de86324a450ab7cb9189
Magnus Aagaard <magnus_aagaard@hotmail.com>
2022-01-19 14:50:34 +0100

    Added return value from offscreen renderer.

--- src/renderer.py
+++ src/renderer.py
@@ -19,13 +19,19 @@ class Renderer:
         Rz = utils.get_rotation_matrix('z', -np.pi/2)
         camera_pose = np.array([[1.0, 0, 0, -30.0],
                                 [0.0, 1.0, 0.0, 0.0],
-                                [0.0, 0.0, 1.0, 65.0],
+                                [0.0, 0.0, 1.0, 65.0 + 8.043],
                                 [0.0, 0.0, 0.0, 1.0]])
         camera_pose[:3,:3] = Rz @ Rx @ camera_pose[:3,:3]
         scene.add(camera, pose=camera_pose)
         pyrender.Viewer(scene, use_raymond_lighting=True)
 
-    def offscreen_render(self):
+    def offscreen_render(self, estimate):
+        '''
+        Renders CAD model to 2D image with the camera looking straight at the model
+        with a position of x,y,z given from parameters 'estimate'.
+        Returns the rendered 2D image.
+        '''
+        x, y, z = estimate
         _trimesh = trimesh.load(self.file_path, file_type='stl')
         mesh = pyrender.Mesh.from_trimesh(_trimesh)
         scene = pyrender.Scene()
@@ -34,19 +40,17 @@ class Renderer:
         camera = pyrender.IntrinsicsCamera(fx=554.920125, fy=554.921917, cx=320.077433, cy=239.661438, znear=0.1, zfar=500)
         Rx = utils.get_rotation_matrix('x', np.pi/2)
         Rz = utils.get_rotation_matrix('z', -np.pi/2)
-        camera_pose = np.array([[1.0, 0, 0, -300.0],
-                                [0.0, 1.0, 0.0, 0.0],
-                                [0.0, 0.0, 1.0, 65.0],
+        camera_pose = np.array([[1.0, 0, 0, x],
+                                [0.0, 1.0, 0.0, y],
+                                [0.0, 0.0, 1.0, z],
                                 [0.0, 0.0, 0.0, 1.0]])
         camera_pose[:3,:3] = Rz @ Rx @ camera_pose[:3,:3]
         scene.add(camera, pose=camera_pose)
-        light = pyrender.DirectionalLight(intensity=3.0)
-        scene.add(light, pose=camera_pose)
+        #light = pyrender.DirectionalLight(intensity=3.0)
+        #scene.add(light, pose=camera_pose)
         r = pyrender.OffscreenRenderer(640, 480)
         color, depth = r.render(scene)
-        
-        cv2.imshow('Image', color)
-        cv2.waitKey(0)
+        return color
 
 def main():
     renderer = Renderer('/home/magnus/master_thesis/catkin_ws/src/autonomous_turbine_inspection/models/Vestas_V52/meshes/vestas_v52.stl')
