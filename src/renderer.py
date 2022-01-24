84086893444c80ee2ae85a781b4b3f32faa07a90
Magnus Aagaard <magnus_aagaard@hotmail.com>
2022-01-24 13:54:55 +0100

    Cleaned up code.

--- src/renderer.py
+++ src/renderer.py
@@ -25,18 +25,8 @@ class Renderer:
         self.r = pyrender.OffscreenRenderer(640, 480)
 
     def render_and_show(self):
-        #_trimesh = trimesh.load(self.file_path, file_type='stl')
-        #mesh = pyrender.Mesh.from_trimesh(_trimesh)
         scene = pyrender.Scene()
         scene.add(self.mesh)
-        #camera = pyrender.PerspectiveCamera(yfov=1.047, aspectRatio=1.333)
-        #camera = pyrender.IntrinsicsCamera(fx=554.920125, fy=554.921917, cx=320.077433, cy=239.661438, znear=0.1, zfar=500)
-        #Rx = utils.get_rotation_matrix('x', np.pi/2)
-        #Rz = utils.get_rotation_matrix('z', -np.pi/2)
-        #camera_pose = np.array([[1.0, 0, 0, -30.0],
-        #                        [0.0, 1.0, 0.0, 0.0],
-        #                        [0.0, 0.0, 1.0, 65.0 + 8.043],
-        #                        [0.0, 0.0, 0.0, 1.0]])
         self.camera_pose[:3, 3] = [-30.0, 0.0, 65.0 + 8.043]
         self.camera_pose[:3,:3] = self.Rz @ self.Rx @ self.camera_pose[:3,:3]
         scene.add(self.camera, pose=self.camera_pose)
@@ -48,19 +38,8 @@ class Renderer:
         with a position of x,y,z given from parameters 'estimate'.
         Returns the rendered 2D image.
         '''
-        #x, y, z = estimate
-        #_trimesh = trimesh.load(self.file_path, file_type='stl')
-        #mesh = pyrender.Mesh.from_trimesh(_trimesh)
         scene = pyrender.Scene()
         scene.add(self.mesh)
-        #camera = pyrender.PerspectiveCamera(yfov=1.047, aspectRatio=1.333)
-        #camera = pyrender.IntrinsicsCamera(fx=554.920125, fy=554.921917, cx=320.077433, cy=239.661438, znear=0.1, zfar=500)
-        #Rx = utils.get_rotation_matrix('x', np.pi/2)
-        #Rz = utils.get_rotation_matrix('z', -np.pi/2)
-        #camera_pose = np.array([[1.0, 0, 0, x],
-        #                        [0.0, 1.0, 0.0, y],
-        #                        [0.0, 0.0, 1.0, z],
-        #                        [0.0, 0.0, 0.0, 1.0]])
         cam_pose = np.copy(self.camera_pose)
         cam_pose[:3, 3] = estimate
         cam_pose[:3,:3] = self.Rz @ self.Rx @ self.camera_pose[:3,:3]
