97f5fc375555d10fb0a67d619ae64c167eaf026b
Magnus Aagaard <magnus_aagaard@hotmail.com>
2022-02-14 16:45:19 +0100

    Now runs validation and logs to TensorBoard. Also only applies data augmentation on training data.

--- src/hourglass_network/dataloader.py
+++ src/hourglass_network/dataloader.py
@@ -1,22 +1,23 @@
 import os
 from torch.utils.data import Dataset, DataLoader
-from torchvision.transforms import Compose, ToTensor, RandomCrop, Resize, CenterCrop
+from torchvision.transforms import Compose, ToTensor, CenterCrop
 import json
 import numpy as np
 import preprocessing
 
 class WindturbineDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None):
+    def __init__(self, annotations_file, img_dir, transform=None, train=True):
         self.annotations = preprocessing.get_annotations(annotations_file)
         self.img_dir = img_dir
         self.transform = transform
+        self.train = train
 
     def __len__(self):
         return len(self.annotations)
 
     def __getitem__(self, idx):
         img = self.annotations[idx]
-        input_img, label_img = preprocessing.process_annotations(img, apply_augmentation=True)
+        input_img, label_img = preprocessing.process_annotations(img, apply_augmentation=self.train)
         if self.transform:
             input_img = self.transform(input_img)
             label_img = self.transform(label_img)
--- src/hourglass_network/train.py
+++ src/hourglass_network/train.py
@@ -1,33 +1,38 @@
 import os
 import sys
 from tqdm import tqdm
-import torch
-from torch.optim import Adam
-from torch.nn import BCELoss
-from torch.utils.data import DataLoader
-from torchvision.transforms import Compose, ToTensor, RandomCrop, Resize, CenterCrop
 import numpy as np
 import argparse
 import shutil
+
+import torch
+from torch.optim import Adam
+from torch.nn import BCELoss
+from torch.utils.data import DataLoader, random_split
+from torchvision.transforms import Compose, ToTensor, RandomCrop, Resize, CenterCrop
+from torch.utils.tensorboard import SummaryWriter
+import matplotlib.pyplot as plt
 
 from model import ConvEncoderDecoder
 from dataloader import WindturbineDataset
 
 def parse_command_line():
         parser = argparse.ArgumentParser()
-        parser.add_argument('-s', '--save', type=int, default=5, help='save checkpoint of model every x epoch')
-        parser.add_argument('-e', '--epochs', type=int, default=150, help='max number of epochs')
+        parser.add_argument('-e', '--epochs', type=int, default=500, help='max number of epochs')
         parser.add_argument('-r', '--resume', type=int, default=0, help='whether to resume training from a checkpoint. Provide epoch number.')
         parser.add_argument('-b', '--base_dir', type=str, default='./src/hourglass_network', help='base directory of model code')
+        parser.add_argument('-v', '--validate', type=int, default=1, help='number of epochs between model validation. Also saves best model when validating.')
         args = parser.parse_args()
         return args
 
 class Trainer:
     def __init__(self, args):
         self.num_epochs = args.epochs
-        self.save_interval = args.save
+        self.validate_interval = args.validate
         self.resume = args.resume
         self.base_dir = args.base_dir
+        # TensorBoard writer
+        self.writer = SummaryWriter(self.base_dir + '/runs/augmentation_experiment_1')
         # Use CUDA if available
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.model = ConvEncoderDecoder(10)
@@ -79,17 +84,76 @@ class Trainer:
             'epoch': self.epoch,
             'loss': self.lowest_loss}, is_best)
         
+    # helper function to plot images to TensorBoard
+    def plot_preds(self, images):
+        outputs = self.model(images)
+        fig = plt.figure(figsize=(15, 15))
+        for idx in np.arange(images.shape[0]):
+            # Remove expanded dim, move to cpu and numpyfi
+            output = torch.squeeze(outputs[idx]).cpu().numpy().transpose(1,2,0)
+            input_img_data = torch.squeeze(images[idx]).cpu().numpy().transpose(1,2,0)
+            ax = fig.add_subplot(images.shape[0],4,4*idx+1,xticks=[],yticks=[])
+            input_img = input_img_data[:,:,:3]
+            plt.imshow(input_img[...,::-1])
+            ax.set_title("Input")
+            ax = fig.add_subplot(images.shape[0],4,4*idx+2,xticks=[],yticks=[])
+            output_img = output[:,:,:3]
+            plt.imshow(output_img[...,::-1])
+            ax.set_title("Output")
+            ax = fig.add_subplot(images.shape[0],4,4*idx+3,xticks=[],yticks=[])
+            plt.imshow(np.sum(output[:,:,3:7], axis=2), cmap='gray', vmin=0, vmax=1.0)
+            ax.set_title("Points")
+            ax = fig.add_subplot(images.shape[0],4,4*idx+4,xticks=[],yticks=[])
+            plt.imshow(np.sum(output[:,:,7:], axis=2), cmap='gray', vmin=0, vmax=1.0)
+            ax.set_title("Lines")
+        return fig
+    
+    def evaluate(self):
+        # Evaluate model and return validation loss
+        val_loss = 0.0
+        img_plotted = False
+        self.model.eval()
+        self.val_set.dataset.train = False
+        with torch.no_grad():
+            for data in self.val_dataloader:
+                input_images, label_images = data
+                input_images = input_images.to(self.device)
+                label_images = label_images.to(self.device)
+                outputs = self.model(input_images)
+                loss = self.criterion(outputs, label_images)
+                val_loss += loss.item()*input_images.size(0)
+                if not img_plotted:
+                    fig = self.plot_preds(input_images[:4])
+                    self.writer.add_figure('validation figures', fig, self.epoch)
+                    img_plotted = True
+            
+        val_loss = val_loss/len(self.val_dataloader)
+        self.writer.add_scalar('validation loss', val_loss, self.epoch)
+        tqdm.write('Epoch: {} \tValidation Loss: {:.6f}'.format(self.epoch, val_loss))
+        return val_loss
+        
     def train(self):
         # Run trainer
-        train_dataset = WindturbineDataset(f'{self.base_dir}/data/annotations.json', f'{self.base_dir}/data/all_data', transform=Compose([ToTensor(), CenterCrop(256)]))
-        train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+        dataset = WindturbineDataset(f'{self.base_dir}/data/annotations.json', f'{self.base_dir}/data/all_data', transform=Compose([ToTensor(), CenterCrop(256)]))
+        train_val_split = int(len(dataset)*0.8)
+        self.train_set, self.val_set = random_split(dataset, [train_val_split, len(dataset)-train_val_split], generator=torch.Generator().manual_seed(42))
+        print(f'Length of train dataset: {len(self.train_set)} \t Length of val dataset: {len(self.val_set)}')
+        self.train_dataloader = DataLoader(self.train_set, batch_size=4, num_workers=8, shuffle=True)
+        self.val_dataloader = DataLoader(self.val_set, batch_size=4, num_workers=8, shuffle=False)
+        
+        # Write model to TensorBoard
+        input_to_model, _ = next(iter(self.train_set))
+        input_to_model = torch.unsqueeze(input_to_model, 0).to(self.device)
+        self.writer.add_graph(self.model, input_to_model)
         
         for self.epoch in tqdm(range(self.epoch+1, self.num_epochs+1)):
             # monitor training loss
             train_loss = 0.0
+            self.model.train()
+            self.train_set.dataset.train = True
             best = False
             #Training
-            for data in tqdm(train_dataloader):
+            for data in self.train_dataloader:
                 input_images, label_images = data
                 input_images = input_images.to(self.device)
                 label_images = label_images.to(self.device)
@@ -100,11 +164,13 @@ class Trainer:
                 self.optimizer.step()
                 train_loss += loss.item()*input_images.size(0)
                 
-            train_loss = train_loss/len(train_dataloader)
+            train_loss = train_loss/len(self.train_dataloader)
+            self.writer.add_scalar('training loss', train_loss, self.epoch)
             tqdm.write('Epoch: {} \tTraining Loss: {:.6f}'.format(self.epoch, train_loss))
-            if self.epoch % self.save_interval == 0:
-                if train_loss < self.lowest_loss:
-                    self.lowest_loss = train_loss
+            if self.epoch % self.validate_interval == 0:
+                val_loss = self.evaluate()
+                if val_loss < self.lowest_loss:
+                    self.lowest_loss = val_loss
                     best = True
                 self.save(is_best=best)
 
