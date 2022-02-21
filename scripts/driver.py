915e5570b14d89aa40bbd3fb69fad5e4bd51f880
Magnus Aagaard <magnus_aagaard@hotmail.com>
2022-02-21 16:50:08 +0100

    Updated driver to run with new chamfer matcher

--- scripts/driver.py
+++ scripts/driver.py
@@ -9,7 +9,7 @@ import numpy as np
 def main():
     render = Renderer(tower='./models/vestas_v52_rotation/meshes/vestas_v52_tower.stl', wings='./models/vestas_v52_rotation/meshes/vestas_v52_wings.stl')
     img = cv2.imread('./scripts/image_data/gazebo_100_45.png')
-    cv2.imshow('Image', img)
+    #cv2.imshow('Image', img)
     bd = BladeDetector(img_path='./scripts/image_data/gazebo_100_45.png', save_result=True)
     bd.get_mask('./scripts/image_data/offshore_wind_turbine.jpg', './scripts/image_data/annotated_wind_turbine.jpg')
     model_dict = bd.detect()
@@ -18,36 +18,26 @@ def main():
     #angles_between_lines = model_dict.get('angles_between_lines')
     #print(f'Angle between blades: {angles_between_lines}')
     # D' = (known_width * focal_length / pixels) + length of turbine nacelle (we want distance to center of turbine)
-    estimated_dist = (35.1 * 554.920125) / mean_length + 5.16
+    #estimated_dist = (35.1 * 554.920125) / mean_length + 5.16
+    estimated_dist = 100
     print(f'Estimated distance: {estimated_dist}')
-    # Chamfer matcher needs an initial template..
-    template = render.offscreen_render([-estimated_dist, 0, 71.74 - 8])
-    cv2.imshow('Template', template)
-    cv2.waitKey(0)
-    cm = ChamferMatcher(img, template)
-    
-    min_x = int(estimated_dist) - 25
-    max_x = int(estimated_dist) + 25
-    min_z = 71 - 8
-    max_z = 71 + 8
-    scores = []
-    estimates = []
-    
-    for x in range(min_x, max_x, 2):
-        for z in range(min_z, max_z, 1):
-            cm.template = render.offscreen_render([-x, 0, z])
-            cm.detect_edges()
-            scores.append(cm.match())
-            estimates.append([-x,0,z])
-    print(np.argmin(scores, axis=0)[0])
-    print(estimates[np.argmin(scores, axis=0)[0]])
-    top_left = scores[np.argmin(scores, axis=0)[0]][1]
-    bottom_right = scores[np.argmin(scores, axis=0)[0]][2]
-    template = render.offscreen_render(estimates[np.argmin(scores, axis=0)[0]])
-    cv2.rectangle(img, top_left, bottom_right, 255, 2)
-    cv2.imshow('Image', img)
-    cv2.imshow('Template', template)
-    cv2.waitKey(0)
+    cm = ChamferMatcher(img, render)
+    # Base estimates: UAV located at tower height.
+    # Wind turbine located directly in front in the middle of the image with wings oriented
+    init_x = -100
+    init_y = 0
+    init_z = 74
+    init_roll = 20
+    init_yaw = 0
+    init_est = [init_x, init_y, init_z, init_roll, init_yaw]
+    best_estimate = cm.run_optimization(init_est, 5, show_plots=True)
+    x = -best_estimate[0]
+    y = best_estimate[1]
+    z = best_estimate[2]
+    roll = best_estimate[3]
+    yaw = best_estimate[4]
+    stm = SkeletalTurbineModel(c=(x,y), h=z, omega=np.pi + np.deg2rad(yaw), phi=np.deg2rad(60 + roll))
+    stm.plot_model()
 
 if __name__ == "__main__":
     main()
