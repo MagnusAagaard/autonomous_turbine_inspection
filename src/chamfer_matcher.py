ca42d6f3c0745e7d1c7bd2de0952a73cbcc13f21
Magnus Aagaard <magnus_aagaard@hotmail.com>
2022-03-14 10:15:02 +0100

    Added inverse rotation tests for optimization. Removed y optimization. Added visualization of different steps in pipeline.

--- src/chamfer_matcher.py
+++ src/chamfer_matcher.py
@@ -3,6 +3,7 @@ import numpy as np
 import timeit
 import matplotlib.pyplot as plt
 from copy import copy
+from PIL import Image
 
 from renderer import Renderer
 import utils
@@ -11,11 +12,20 @@ class ChamferMatcher:
     def __init__(self, img, renderer):
         self.img_color = copy(img)
         self.img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        self.img_edges = cv2.Canny(self.img,5,50, apertureSize=3, L2gradient=True)
+        self.img_edges = cv2.Canny(self.img,50,100, apertureSize=3, L2gradient=True)
+        self.detect_and_remove_horizontal_lines(self.img_edges)
         self.dist_img = cv2.distanceTransform(255 - self.img_edges, cv2.DIST_L1, 3).astype(np.uint8)
         self.renderer = renderer
-        self.template = cv2.cvtColor(self.renderer.offscreen_render([-100, 0, 65 + 8, 30, 45]), cv2.COLOR_BGR2GRAY)
+        self.template = cv2.cvtColor(self.renderer.offscreen_render([-100, -0.8, 66 + 8, 6, 40]), cv2.COLOR_BGR2GRAY)
         self.detect_edges()
+        self.match(init=True)
+        _, top_left, bottom_right = self.match()
+        vis_img = cv2.cvtColor(self.dist_img.copy(), cv2.COLOR_GRAY2RGB)
+        edges = np.argwhere(self.template_edges == 255)
+        for pt in edges:
+            vis_img[top_left[1]+pt[0], top_left[0]+pt[1],:] = np.array([255,0,0])
+        template = Image.fromarray(vis_img)
+        template.save('/home/magnus/chamfer_matcher_dist_img.pdf')
     
     def render_new_template(self, estimate):
         '''
@@ -30,12 +40,30 @@ class ChamferMatcher:
         '''
         Detect edges in the template image and crop to minimum bounding box.
         '''
-        self.template_edges = cv2.Canny(self.template,5,50, apertureSize=3, L2gradient=True)
+        self.template_edges = cv2.Canny(self.template,50,100, apertureSize=3, L2gradient=True)
         # Crop image by minimum bounding box
         self.template_edges = self.template_edges[~np.all(self.template_edges == 0, axis=1)]
         self.template_edges = self.template_edges[:, ~np.all(self.template_edges == 0, axis=0)]
         
-    def match(self):
+    def detect_and_remove_horizontal_lines(self, img):
+        '''
+        Detects longest horizontal line (horizon) and removes it. (if it is there..)
+        '''
+        result = self.img_color.copy()
+        horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (40,1))
+        detect_horizontal = cv2.morphologyEx(img, cv2.MORPH_OPEN, horizontal_kernel, iterations=2)
+        cnts = cv2.findContours(detect_horizontal, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+        for c in cnts:
+            pt1 = np.array(c)[0].flatten()
+            pt2 = np.array(c)[1].flatten()
+            #print(np.linalg.norm(pt1-pt2))
+            pt1[0] -= 1000
+            pt2[0] += 1000
+            #cv2.drawContours(result, (), -1, (36,255,12), 2)
+            cv2.line(img, pt1, pt2, 0, 2)
+        
+    def match(self, init=False):
         '''
         Performs chamfer matching using distance image.
         '''
@@ -67,6 +95,21 @@ class ChamferMatcher:
         #cv2.imshow('Match result', res)
         #cv2.imshow('Image', self.img)
         #cv2.waitKey(5)
+        if init:
+            im1 = Image.fromarray(cv2.cvtColor(self.img_color, cv2.COLOR_BGR2RGB))
+            im1.save('/home/magnus/chamfer_matcher_color.pdf')
+            im2 = Image.fromarray(self.img)
+            im2.save('/home/magnus/chamfer_matcher_gray.pdf')
+            im3 = Image.fromarray(self.img_edges)
+            im3.save('/home/magnus/chamfer_matcher_edges.pdf')
+            img5 = cv2.cvtColor(cv2.normalize(res, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U), cv2.COLOR_GRAY2RGB)
+            cv2.circle(img5, min_loc, 5, (255,0,255), -1)
+            cv2.rectangle(self.img_color, top_left, bottom_right, (0,255,0), 2)
+            im5 = Image.fromarray(img5)
+            im5.save('/home/magnus/chamfer_matcher_result.pdf')
+            im6 = Image.fromarray(cv2.cvtColor(self.img_color, cv2.COLOR_BGR2RGB))
+            im6.save('/home/magnus/chamfer_matcher_color_result.pdf')
+            
         return min_val, top_left, bottom_right
     
     def run_optimization(self, init_est, max_iterations, show_plots=True):
@@ -78,7 +121,7 @@ class ChamferMatcher:
         '''
         # Define ranges to search for on each side of init estimate
         x_range = 5
-        y_range = 1
+        y_range = 0
         z_range = 0
         roll_range = 30
         yaw_range = 180
@@ -88,11 +131,15 @@ class ChamferMatcher:
         best_xi = copy(init_est)
         # First optimize roll and yaw without x,y,z..
         best_xi = self.optimize(opt_scores, best_xi, [x_range, y_range, z_range, roll_range, yaw_range], index=3)
-        print(best_xi)
+        print(f'Before inverse rotation test: {best_xi}')
+        best_xi = self.test_inverse_rotations(best_xi)
+        print(f'After inverse rotation test: {best_xi}')
         # Run optimization. Stop if max_iterations is reached or change between parameters is small
         for i in range(max_iterations):
             new_best_xi = self.optimize(opt_scores, best_xi, [x_range, y_range, z_range, roll_range, yaw_range])
-            print(new_best_xi)
+            print(f'Before inverse rotation test: {new_best_xi}')
+            new_best_xi = self.test_inverse_rotations(new_best_xi)
+            print(f'After inverse rotation test: {new_best_xi}')
             if np.linalg.norm(np.asarray(best_xi)-np.asarray(new_best_xi)) < 5:
                 print(np.linalg.norm(np.asarray(best_xi)-np.asarray(new_best_xi)))
                 best_xi = new_best_xi
@@ -174,6 +221,36 @@ class ChamferMatcher:
             return self.optimize(opt_scores, best_xi, est_range, index=index+1)
         return best_xi
     
+    def test_inverse_rotations(self, xi):
+        self.render_new_template(xi)
+        self.detect_edges()
+        f_xi = self.match()
+        inv_xi = copy(xi)
+        min_score = f_xi
+        min_xi = copy(xi)
+        # First test yaw with same roll
+        for i in range(3):
+            inv_xi[4] += 90
+            self.render_new_template(inv_xi)
+            self.detect_edges()
+            f_inv_xi = self.match()
+            if f_inv_xi < min_score:
+                min_score = f_inv_xi
+                min_xi = copy(inv_xi)
+        # Roll 180 degrees and test yaws again
+        inv_xi = copy(xi)
+        inv_xi[3] += 180
+        for i in range(3):
+            inv_xi[4] += 90
+            self.render_new_template(inv_xi)
+            self.detect_edges()
+            f_inv_xi = self.match()
+            if f_inv_xi < min_score:
+                min_score = f_inv_xi
+                min_xi = copy(inv_xi)
+        return min_xi
+        
+    
     def refine_optimization(self, params):
         '''
         Refines the estimated parameters of the wind turbine.
@@ -186,21 +263,22 @@ class ChamferMatcher:
         estimates = []
         print(params)
         for x in range(params[0]-3, params[0]+3, 1):
-            for y in np.arange(params[1] - 0.2, params[1] + 0.2, 0.1):
-                print(y)
-                for z in range(params[2]-2, params[2]+2, 1):
-                    for roll in range(params[3]-2, params[3]+2, 1):
-                        for yaw in range(params[4]-2, params[4]+2, 1):
-                            self.render_new_template([x,y,z,roll,yaw])
-                            self.detect_edges()
-                            scores.append(self.match())
-                            estimates.append([x,y,z,roll,yaw])
+            y=0
+            #for y in np.arange(params[1] - 0.2, params[1] + 0.2, 0.1):
+                #print(y)
+            for z in range(params[2]-2, params[2]+2, 1):
+                for roll in range(params[3]-2, params[3]+2, 1):
+                    for yaw in range(params[4]-2, params[4]+2, 1):
+                        self.render_new_template([x,y,z,roll,yaw])
+                        self.detect_edges()
+                        scores.append(self.match())
+                        estimates.append([x,y,z,roll,yaw])
         
         idx = np.argmin(scores, axis=0)[0]
         return scores[idx], estimates[idx]
 
 def main():
-    img = cv2.imread('./scripts/image_data/gazebo_100_45.png')
+    img = cv2.imread('./scripts/image_data/gazebo_100_90.png')
     render = Renderer(tower='./models/vestas_v52_rotation/meshes/vestas_v52_tower.stl', wings='./models/vestas_v52_rotation/meshes/vestas_v52_wings.stl')
     #print('render\t\t', timeit.timeit(lambda: render.offscreen_render([-100, 0, 65 + 8]), number=300) / 300)
     cm = ChamferMatcher(img, render)
