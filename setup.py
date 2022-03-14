8444d6187b6b262d1141eaa926c0c782107d5687
Magnus Aagaard <magnus_aagaard@hotmail.com>
2022-03-14 12:25:25 +0100

    Moved optimizer

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['blade_detector', 'renderer', 'chamfer_matcher', 'skeletal_turbine_model','hourglass_network'],
+    packages=['blade_detector', 'renderer', 'chamfer_matcher', 'skeletal_turbine_model','hourglass_network', 'optimizer'],
     package_dir={'': 'src'}
 )
 
