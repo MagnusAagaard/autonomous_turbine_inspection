d7712d98c065c50f9dda37923e108284c66f6d4d
Magnus Aagaard <magnus_aagaard@hotmail.com>
2022-04-20 13:16:53 +0200

    Added quaternion_from_matrix function

--- src/utils.py
+++ src/utils.py
@@ -142,9 +142,9 @@ def quarternion_to_rotation_matrix_g2o(q, inverse=False):
 
 def quarternion_to_rotation_matrix_g2o_cam_frame(q, inverse=False):
     qw = q.w()
-    qx = -q.y() if not inverse else -q.x()
-    qy = -q.z() if not inverse else -q.y()
-    qz = q.x() if not inverse else -q.z()
+    qx = -q.y() if not inverse else q.y()
+    qy = -q.z() if not inverse else q.z()
+    qz = q.x() if not inverse else -q.x()
     R11 = 1 - 2*qy**2 - 2*qz**2	
     R12 = 2*qx*qy - 2*qz*qw
     R13 = 2*qx*qz + 2*qy*qw
@@ -292,6 +292,37 @@ def euler_from_matrix(matrix, axes='sxyz'):
     if frame:
         ax, az = az, ax
     return ax, ay, az
+
+def quaternion_from_matrix(matrix):
+    """Return quaternion from rotation matrix.
+
+    >>> R = rotation_matrix(0.123, (1, 2, 3))
+    >>> q = quaternion_from_matrix(R)
+    >>> numpy.allclose(q, [0.0164262, 0.0328524, 0.0492786, 0.9981095])
+    True
+
+    """
+    q = np.empty((4, ), dtype=np.float64)
+    M = np.array(matrix, dtype=np.float64, copy=False)[:4, :4]
+    t = np.trace(M)
+    if t > M[3, 3]:
+        q[3] = t
+        q[2] = M[1, 0] - M[0, 1]
+        q[1] = M[0, 2] - M[2, 0]
+        q[0] = M[2, 1] - M[1, 2]
+    else:
+        i, j, k = 0, 1, 2
+        if M[1, 1] > M[0, 0]:
+            i, j, k = 1, 2, 0
+        if M[2, 2] > M[i, i]:
+            i, j, k = 2, 0, 1
+        t = M[i, i] - (M[j, j] + M[k, k]) + M[3, 3]
+        q[i] = t
+        q[j] = M[i, j] + M[j, i]
+        q[k] = M[k, i] + M[i, k]
+        q[3] = M[k, j] - M[j, k]
+    q *= 0.5 / math.sqrt(t * M[3, 3])
+    return q
 
 def dist_between_points(self, p1, p2):
     '''
